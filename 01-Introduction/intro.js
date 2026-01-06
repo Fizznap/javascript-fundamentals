0ff17a5a46faafd2516215fb5bfa905ca9188aa0
Nyxee <shindesmit314@gmail.com>
2026-01-06 23:49:02 +0530

    02-operators and comments with updated readme

--- 01-Introduction/intro.js
+++ 01-Introduction/intro.js
@@ -3,4 +3,5 @@ alert("This is an Warning Alert!"); // This line displays a warning alert messag
 
 console.log("Hello, World!"); // This line prints a message to the console
 
+
 console.log("My Name is Smit")
