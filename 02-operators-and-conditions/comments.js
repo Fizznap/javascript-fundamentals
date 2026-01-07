7a08c078538107b43e968210eb2ed6728443213a
Nyxee <shindesmit314@gmail.com>
2026-01-08 01:12:39 +0530

    Lecture 02 is done

---
+++ 02-operators-and-conditions/comments.js
@@ -0,0 +1,9 @@
+
+//console.log("This is a single-line comment example.");
+
+/*
+Hello My name is Smit!!
+I am learning JavaScript.
+This is a multi-line comment example.
+*/
+console.log("Comments are useful for explaining code and making it more readable.");
